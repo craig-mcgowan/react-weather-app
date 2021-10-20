fb409e90578345568fdcfebebd23a395ca6b4a69
Craig McGowan <craigmcgowan15@gmail.com>
2021-10-19 22:11:24 -0400

    completes lab, bonus still to come

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
 import "./styles.css";
 import weatherData from "./weatherData";
+import WeatherForecast from "./WeatherForecast"
 console.log(weatherData)
 export default function App() {
+  const forecasts = weatherData.map((forecast, index) => (
+    <WeatherForecast {...forecast} key={index}/>
+  ))
   return (
     <div className="App">
-      <h1>My WeatherIcons App</h1> 
+      <h1>My WeatherIcons App</h1>
+      <section>
+        {forecasts} 
+      </section>
+      
+
     </div>
   );
 }
